a25ecb81dd472aba53b75601338149a70cf92d06
Baia <baiagrdzelishvili@gmail.com>
2026-03-18 16:59:11 -0400

    First attempt to create a game.

---
+++ data/alphabet.py
@@ -0,0 +1,62 @@
+alphabet = {
+    "ა": {
+        "latin": "a (A)",
+        "letter_audio": "audio/letters/a.wav",
+
+        "example": {
+            "word": "ანა",
+            "meaning": "Ana (name)",
+            "audio": "audio/examples/ananas.wav",
+            "image": "images/examples/ananas.png"
+        }
+    },
+
+    "ბ": {
+        "latin": "b (B)",
+        "letter_audio": "audio/letters/b.wav",
+
+        "example": {
+            "word": "ბუშტი",
+            "meaning": "balloon",
+            "audio": "audio/examples/balloon.wav",
+            "image": "images/examples/balloon.png"
+        }
+    },
+
+    "გ": {
+        "latin": "g (G)",
+        "letter_audio": "audio/letters/g.wav",
+
+        "example": {
+            "word": "გოლი (მიზანი)",
+            "meaning": "Goal",
+            "audio": "audio/examples/goal.wav",
+            "image": "images/examples/goal.png"
+        }
+    },
+
+    "დ": {
+        "latin": "d (D)",
+        "letter_audio": "audio/letters/d.wav",
+
+        "example": {
+            "word": "ბრილიანტი",
+            "meaning": "Diamond",
+            "audio": "audio/examples/diamond.wav",
+            "image": "images/examples/diamond.png"
+        }
+    },
+
+    "ე": {
+        "latin": "e (E)",
+        "letter_audio": "audio/letters/e.wav",
+
+        "example": {
+            "word": "კვერცხი",
+            "meaning": "Egg",
+            "audio": "audio/examples/egg.wav",
+            "image": "images/examples/egg.png"
+        }
+    }
+
+}
