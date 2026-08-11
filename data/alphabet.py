9fdc63df352f836288649525898eb97fab2545b6
baia28 <baiagrdzelishvili@gmail.com>
2026-08-11 11:09:23 -0400

    Clearing up files before working on Alphabet Code migration.

--- data/alphabet.py
+++ data/alphabet.py
@@ -1,367 +1,367 @@
 alphabet = {
     "ა": {
         "latin": "a (A)",
-        "letter_audio": "audio/letters/a.wav",
+        "letter_audio": "audio/letters/a.m4a",
 
         "example": {
             "word": "ანანასი",
             "meaning": "Pineapple",
-            "audio": "audio/examples/ananas.wav",
+            "audio": "audio/examples/ananas.m4a",
             "image": "images/examples/ananas.png"
         }
     },
 
     "ბ": {
         "latin": "b (B)",
-        "letter_audio": "audio/letters/b.wav",
+        "letter_audio": "audio/letters/b.m4a",
 
         "example": {
             "word": "ბუშტი",
             "meaning": "balloon",
-            "audio": "audio/examples/balloon.wav",
+            "audio": "audio/examples/balloon.m4a",
             "image": "images/examples/balloon.png"
         }
     },
 
     "გ": {
         "latin": "g (G)",
-        "letter_audio": "audio/letters/g.wav",
+        "letter_audio": "audio/letters/g.m4a",
 
         "example": {
             "word": "გოლი (მიზანი)",
             "meaning": "Goal",   # guitar
-            "audio": "audio/examples/goal.wav",
+            "audio": "audio/examples/goal.m4a",
             "image": "images/examples/goal.png"
         }
     },
 
     "დ": {
         "latin": "d (D)",
-        "letter_audio": "audio/letters/d.wav",
+        "letter_audio": "audio/letters/d.m4a",
 
         "example": {
             "word": "დათვი",
             "meaning": "Bear",
-            "audio": "audio/examples/bear.wav",  #დათვი
+            "audio": "audio/examples/bear.m4a",  #დათვი
             "image": "images/examples/bear.png"
         }
     },
 
     "ე": {
         "latin": "e (E)",
-        "letter_audio": "audio/letters/e.wav",
+        "letter_audio": "audio/letters/e.m4a",
 
         "example": {
             "word": "კვერცხი",
             "meaning": "Egg",
-            "audio": "audio/examples/egg.wav",
+            "audio": "audio/examples/egg.m4a",
             "image": "images/examples/egg.png"
         }
     },
         "ვ": {
         "latin": "v (V)",
-        "letter_audio": "audio/letters/v.wav",
+        "letter_audio": "audio/letters/v.m4a",
         "example": {
             "word": "ვიოლინო",
             "meaning": "Violin",
-            "audio": "audio/examples/violin.wav",
+            "audio": "audio/examples/violin.m4a",
             "image": "images/examples/violin.png"
         }
     },
 
     "ზ": {
         "latin": "z (Z)",
-        "letter_audio": "audio/letters/z.wav",
+        "letter_audio": "audio/letters/z.m4a",
         "example": {
             "word": "ზებრა",
             "meaning": "Zebra",
-            "audio": "audio/examples/zebra.wav",
+            "audio": "audio/examples/zebra.m4a",
             "image": "images/examples/zebra.png"
         }
     },
 
     "თ": {
         "latin": "t (T)",
-        "letter_audio": "audio/letters/t.wav",
+        "letter_audio": "audio/letters/t.m4a",
         "example": {
             "word": "ჩაი",
             "meaning": "Tea",
-            "audio": "audio/examples/tea.wav",
+            "audio": "audio/examples/tea.m4a",
             "image": "images/examples/tea.png"
         }
     },
 
     "ი": {
         "latin": "i (I)",
-        "letter_audio": "audio/letters/i.wav",
+        "letter_audio": "audio/letters/i.m4a",
         "example": {
             "word": "ინტელექტი",
             "meaning": "Intellect",   #intersection symbol   #იდეა #ინტელექტი
-            "audio": "audio/examples/intellect.wav",
+            "audio": "audio/examples/intellect.m4a",
             "image": "images/examples/intellect.png"
         }
     },
 
     "კ": {
         "latin": "k (K)",
-        "letter_audio": "audio/letters/k.wav",
+        "letter_audio": "audio/letters/k.m4a",
         "example": {
             "word": "კატა",
             "meaning": "Cat",
-            "audio": "audio/examples/cat.wav",
+            "audio": "audio/examples/cat.m4a",
             "image": "images/examples/cat.png"
         }
     },
 
     "ლ": {
         "latin": "l (L)",
-        "letter_audio": "audio/letters/l.wav",
+        "letter_audio": "audio/letters/l.m4a",
         "example": {
             "word": "ლამპა",
             "meaning": "Lamp",
-            "audio": "audio/examples/lamp.wav",
+            "audio": "audio/examples/lamp.m4a",
             "image": "images/examples/lamp.png"
         }
     },
 
     "მ": {
         "latin": "m (M)",
-        "letter_audio": "audio/letters/m.wav",
+        "letter_audio": "audio/letters/m.m4a",
         "example": {
             "word": "მთვარე",
             "meaning": "Moon",
-            "audio": "audio/examples/moon.wav",
+            "audio": "audio/examples/moon.m4a",
             "image": "images/examples/moon.png"
         }
     },
 
     "ნ": {
         "latin": "n (N)",
-        "letter_audio": "audio/letters/n.wav",
+        "letter_audio": "audio/letters/n.m4a",
         "example": {
             "word": "ნავი",
             "meaning": "Boat",
-            "audio": "audio/examples/boat.wav",
+            "audio": "audio/examples/boat.m4a",
             "image": "images/examples/boat.png"
         }
     },
 
     "ო": {
         "latin": "o (O)",
-        "letter_audio": "audio/letters/o.wav",
+        "letter_audio": "audio/letters/o.m4a",
         "example": {
             "word": "ბუ",
             "meaning": "Owl",
-            "audio": "audio/examples/owl.wav",  #opera  #organ #office #optics
+            "audio": "audio/examples/owl.m4a",  #opera  #organ #office #optics
             "image": "images/examples/owl.png"
         }
     },
 
     "პ": {
         "latin": "p (P)",
-        "letter_audio": "audio/letters/p.wav",
+        "letter_audio": "audio/letters/p.m4a",
         "example": {
             "word": "პანდა",  # პეპელა   #piano
             "meaning": "Panda", 
-            "audio": "audio/examples/panda.wav",
+            "audio": "audio/examples/panda.m4a",
             "image": "images/examples/panda.png"
         }
     },
 
     "ჟ": {
         "latin": "zh (Zh)",
-        "letter_audio": "audio/letters/zh.wav",
+        "letter_audio": "audio/letters/zh.m4a",
         "example": {
             "word": "ჟირაფი",
             "meaning": "Giraffe",
-            "audio": "audio/examples/giraffe.wav",
+            "audio": "audio/examples/giraffe.m4a",
             "image": "images/examples/giraffe.png"
         }
     },
 
     "რ": {
         "latin": "r (R)",
-        "letter_audio": "audio/letters/r.wav",
+        "letter_audio": "audio/letters/r.m4a",
         "example": {
             "word": "რაკეტა",
             "meaning": "Rocket",
-            "audio": "audio/examples/rocket.wav",
+            "audio": "audio/examples/rocket.m4a",
             "image": "images/examples/rocket.png"
         }
     },
 
     "ს": {
         "latin": "s (S)",
-        "letter_audio": "audio/letters/s.wav",
+        "letter_audio": "audio/letters/s.m4a",
         "example": {
             "word": "სახლი",
             "meaning": "House",
-            "audio": "audio/examples/house.wav",
+            "audio": "audio/examples/house.m4a",
             "image": "images/examples/house.png"
         }
     },
 
     "ტ": {
         "latin": "tʼ (Tʼ)",
-        "letter_audio": "audio/letters/t_strong.wav",
+        "letter_audio": "audio/letters/t_strong.m4a",
         "example": {
             "word": "ტაქსი",
             "meaning": "Taxi",
-            "audio": "audio/examples/taxi.wav",   #taxi #test #technology
+            "audio": "audio/examples/taxi.m4a",   #taxi #test #technology
             "image": "images/examples/taxi.png"
         }
     },
 
     "უ": {
         "latin": "u (U)",
-        "letter_audio": "audio/letters/u.wav",
+        "letter_audio": "audio/letters/u.m4a",
         "example": {
             "word": "უნივერსიტეტი",
             "meaning": "University",    #university
-            "audio": "audio/examples/university.wav",
+            "audio": "audio/examples/university.m4a",
             "image": "images/examples/university.png" 
         }
     },
 
     "ფ": {
         "latin": "f (F)",
-        "letter_audio": "audio/letters/f.wav",
+        "letter_audio": "audio/letters/f.m4a",
         "example": {
             "word": "ფერმა",
             "meaning": "farm",
-            "audio": "audio/examples/farm.wav",  #price   fasi  ფერმა ფოტო
+            "audio": "audio/examples/farm.m4a",  #price   fasi  ფერმა ფოტო
             "image": "images/examples/farm.png" #
         }
     },
 
     "ქ": {
         "latin": "kʼ (Kʼ)",
-        "letter_audio": "audio/letters/k_strong.wav",
+        "letter_audio": "audio/letters/q.m4a",
         "example": {
             "word": "ქათამი",
             "meaning": "Chicken",
-            "audio": "audio/examples/chicken.wav",
+            "audio": "audio/examples/chicken.m4a",
             "image": "images/examples/chicken.png"
         }
     },
 
     "ღ": {
         "latin": "gh",
-        "letter_audio": "audio/letters/gh.wav",
+        "letter_audio": "audio/letters/gh.m4a",
         "example": {
             "word": "ღორი",
             "meaning": "Pig",   #ღრუტ ღრუტ    ღვინო ღმერთი
-            "audio": "audio/examples/pig.wav",
+            "audio": "audio/examples/pig.m4a",
             "image": "images/examples/pig.png" #
         }
     },
 
     "ყ": {
         "latin": "qʼ",
-        "letter_audio": "audio/letters/q.wav",
+        "letter_audio": "audio/letters/k_strong.m4a",
         "example": {
             "word": "ყვავილი",
             "meaning": "Flower",
-            "audio": "audio/examples/flower.wav",
+            "audio": "audio/examples/flower.m4a",
             "image": "images/examples/flower.png"
         }
     },
 
     "შ": {
         "latin": "sh (Sh)",
-        "letter_audio": "audio/letters/sh.wav",
+        "letter_audio": "audio/letters/sh.m4a",
         "example": {
             "word": "შოკოლადი",
             "meaning": "Chocolate",
-            "audio": "audio/examples/chocolate.wav",
+            "audio": "audio/examples/chocolate.m4a",
             "image": "images/examples/chocolate.png"
         }
     },
 
     "ჩ": {
         "latin": "ch (Ch)",
-        "letter_audio": "audio/letters/ch.wav",
+        "letter_audio": "audio/letters/ch.m4a",
         "example": {
             "word": "ჩიტი",
             "meaning": "Bird",   #ჩიპ ჩიპ #ჩაიდანი  #სკამი
-            "audio": "audio/examples/bird.wav",
+            "audio": "audio/examples/bird.m4a",
             "image": "images/examples/bird.png" ###
         }
     },
 
     "ც": {
         "latin": "ts (Ts)",
-        "letter_audio": "audio/letters/ts.wav",
+        "letter_audio": "audio/letters/ts_soft.m4a",
         "example": {
             "word": "ცა",
             "meaning": "Sky",
-            "audio": "audio/examples/sky.wav",
+            "audio": "audio/examples/sky.m4a",
             "image": "images/examples/sky.png"
         }
     },
 
     "ძ": {
         "latin": "dz (Dz)",
-        "letter_audio": "audio/letters/dz.wav",
+        "letter_audio": "audio/letters/dz.m4a",
         "example": {
             "word": "ძაღლი",
             "meaning": "Dog",
-            "audio": "audio/examples/dog.wav",
+            "audio": "audio/examples/dog.m4a",
             "image": "images/examples/dog.png"
         }
     },
 
     "წ": {
         "latin": "tsʼ",
-        "letter_audio": "audio/letters/ts_strong.wav",
+        "letter_audio": "audio/letters/ts_strong.m4a",
         "example": {
             "word": "წიგნი",
             "meaning": "Book",
-            "audio": "audio/examples/book.wav",
+            "audio": "audio/examples/book.m4a",
             "image": "images/examples/book.png"
         }
     },
 
     "ჭ": {
         "latin": "chʼ",
-        "letter_audio": "audio/letters/ch_strong.wav",
+        "letter_audio": "audio/letters/ch_strong.m4a",
         "example": {
             "word": "ჭიქა",
             "meaning": "Cup",
-            "audio": "audio/examples/cup.wav",
+            "audio": "audio/examples/cup.m4a",
             "image": "images/examples/cup.png"
         }
     },
 
     "ხ": {
         "latin": "kh",
-        "letter_audio": "audio/letters/kh.wav",
+        "letter_audio": "audio/letters/kh.m4a",
         "example": {
             "word": "ხე",
             "meaning": "Tree",
-            "audio": "audio/examples/tree.wav",
+            "audio": "audio/examples/tree.m4a",
             "image": "images/examples/tree.png"
         }
     },
 
     "ჯ": {
         "latin": "j (J)",
-        "letter_audio": "audio/letters/j.wav",
+        "letter_audio": "audio/letters/j.m4a",
         "example": {
             "word": "ჯემი",
             "meaning": "Jam",
-            "audio": "audio/examples/jam.wav",
+            "audio": "audio/examples/jam.m4a",
             "image": "images/examples/jam.png"
         }
     },
 
     "ჰ": {
         "latin": "h (H)",
-        "letter_audio": "audio/letters/h.wav",
+        "letter_audio": "audio/letters/h.m4a",
         "example": {
             "word": "ჰამბურგერი",
             "meaning": "Hamburger",
-            "audio": "audio/examples/hamburger.wav",
+            "audio": "audio/examples/hamburger.m4a",
             "image": "images/examples/hamburger.png"
         }
     }
